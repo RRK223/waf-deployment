9aa389bddec199791eea420ae74e75679a995efe
Riwaj Chipalu <69961549+Reewaz-C@users.noreply.github.com>
2026-01-25 10:09:28 +0545

    feat: fixed IAM instance profile

--- terraform/modules/ec2/main.tf
+++ terraform/modules/ec2/main.tf
@@ -26,9 +26,8 @@ resource "aws_instance" "this" {
   instance_type          = var.instance_type
   subnet_id              = var.subnet_id
   vpc_security_group_ids = [var.sg_id]
-  iam_instance_profile   = var.iam_instance_profile
-  key_name = var.key_name
-
+  iam_instance_profile = var.iam_instance_profile_name
+}
   root_block_device {
     volume_type           = var.root_volume_type
     volume_size           = var.root_volume_size

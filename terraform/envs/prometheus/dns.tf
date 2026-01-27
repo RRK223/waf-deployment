e6d30b2532cddc814cc9f1d82c99dcc5338cb097
riwajchipalu <riwazu23@gmail.com>
2026-01-27 10:11:35 +0000

    feat: Fixed record name

--- terraform/envs/prometheus/dns.tf
+++ terraform/envs/prometheus/dns.tf
@@ -1,6 +1,6 @@
 resource "aws_route53_record" "prometheus" {
   zone_id = data.aws_route53_zone.dns.zone_id
-  name    = "promethus.tekbay.click"   
+  name    = "prometheus.tekbay.click"   
   type    = "A"
   ttl     = 300
   records = [module.ec2.public_ip]
